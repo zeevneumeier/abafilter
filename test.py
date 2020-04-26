9d1b34107c1e6ee9d68768a2b0b4897735bc1a84
zeevneumeier <zeev@MacBook-Pro-24.hsd1.ca.comcast.net>
2020-04-26 14:41:01 -0700

    test

--- test.py
+++ test.py
@@ -4,72 +4,40 @@ import syslog
 
 from gtts import gTTS
 from io import BytesIO
-import pygame as pg
+import pygame
+import hashlib
+import pydub
 
-#import pyglet
-
-# The text that you want to convert to audio
-mytext = 'Welcome to geeksforgeeks!'
-  
-# Language in which you want to convert
-language = 'en'
-  
-# Passing the text and language to the engine,
-# here we have marked slow=False. Which tells
-# the module that the converted audio should
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
-  
-# Saving the converted audio in a mp3 file named
-# welcome
-#myobj.save("welcome.mp3")
-
-mp3_fp = BytesIO()
-myobj.write_to_fp(mp3_fp)
-mp3_fp.seek(0)
-  
-# Playing the converted file
-
-
-
-def play_music(music_file, volume=0.8):
-    '''
-    stream music with mixer.music module in a blocking manner
-    this will stream the sound from disk while playing
-    '''
-    # set up the mixer
-    freq = 24000     # audio CD quality
-    bitsize = -16    # unsigned 16 bit
-    channels = 1     # 1 is mono, 2 is stereo
-    buffer = 2048    # number of samples (experiment to get best sound)
-    pg.mixer.init(freq, bitsize, channels, buffer)
-    #pg.init()
-    #pg.mixer.init()
-    # volume value 0.0 to 1.0
+def playNag(message):
     
-    #pg.mixer.music.set_volume(volume)
-    clock = pg.time.Clock()
+    print("nagger.playNag(%s)" % message)
+    
+    filename = "/tmp/abafilter_nag_%s.wav" % hashlib.md5(message.encode('utf-8')).hexdigest()
+    
+    if not os.path.exists(filename):
+        print (filename, "not found. Generating")
+    
+        myobj = gTTS(text=message, lang='en', slow=False)
+        myobj.save("/tmp/abafilter_nag_temp.mp3")
+        sound = pydub.AudioSegment.from_mp3("/tmp/abafilter_nag_temp.mp3")
+        sound.export(filename, format="wav")
+    
+    pygame.mixer.init(24000, -16, 1, 2048)
+    clock = pygame.time.Clock()
 
-    
-    
     try:
-        
-        pg.mixer.music.load(mp3_fp)
-        print("Music file {} loaded!".format(music_file))
-    except pg.error:
-        print("File {} not found! ({})".format(music_file, pg.get_error()))
+        pygame.mixer.music.load(filename)
+    except pygame.error:
+        print("nagger.playNag(%s) ERROR" % message, pygame.get_error())
         return
-    pg.mixer.music.play()
     
+    pygame.mixer.music.play()
     
-    while pg.mixer.music.get_busy():
+    while pygame.mixer.music.get_busy():
         # check if playback has finished
         clock.tick(30)
-# pick a MP3 music file you have in the working folder
-# otherwise give the full file path
-# (try other sound file formats too)
+    
+    print("nagger.playNag(%s) done" % message)
 
-# optional volume 0 to 1.0
-volume = 1
-play_music("welcome.mp3", volume)
 
+playNag("this is a test")
