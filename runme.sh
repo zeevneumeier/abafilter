4f3bf4b9ea1357b586561346a22006c782dd1785
zeevneumeier <zeev@MacBook-Pro-24.hsd1.ca.comcast.net>
2020-04-26 15:00:39 -0700

    path

--- runme.sh
+++ runme.sh
@@ -3,6 +3,9 @@ echo "updating code"
 
 git -C /usr/local/abafilter pull
 
+echo $PATH
+export PATH=$PATH:/usr/local/bin/
+
 echo "running abafilter"
 
 /usr/local/bin/python3 /usr/local/abafilter/abafilter.py /usr/local/abafilter 1c1wIVkNGuluoVmBTPuMH3FKdFnbmJ6i3JDcb8jY7wU8 300 43200
