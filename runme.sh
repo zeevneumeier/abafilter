238ab865dedbc471b20b7e65ea7efb15021241c6
zeevneumeier <zeev@MacBook-Pro-24.hsd1.ca.comcast.net>
2020-04-26 15:04:57 -0700

    and a little more cleanup

--- runme.sh
+++ runme.sh
@@ -6,16 +6,19 @@ git -C /usr/local/abafilter pull
 echo $PATH
 export PATH=$PATH:/usr/local/bin/
 
+NAME="test"
+SHEET="c1wIVkNGuluoVmBTPuMH3FKdFnbmJ6i3JDcb8jY7wU8"
+
 echo "running abafilter"
 
-python3 /usr/local/abafilter/abafilter.py /usr/local/abafilter 1c1wIVkNGuluoVmBTPuMH3FKdFnbmJ6i3JDcb8jY7wU8 300 43200
+python3 /usr/local/abafilter/abafilter.py /usr/local/abafilter $SHEET 300 43200
 
 echo "uploading logs"
 
-python3 /usr/local/abafilter/logsaver.py /usr/local/abafilter/ /tmp/abafilter.out test
+python3 /usr/local/abafilter/logsaver.py /usr/local/abafilter/ /tmp/abafilter.out $NAME
 
 echo "" > /tmp/abafilter.out
 
-python3 /usr/local/abafilter/logsaver.py /usr/local/abafilter/ /tmp/abafilter.err test
+python3 /usr/local/abafilter/logsaver.py /usr/local/abafilter/ /tmp/abafilter.err $NAME
 
 echo "" > /tmp/abafilter.err
